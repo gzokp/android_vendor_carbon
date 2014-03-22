026504c3985e9ff8fbe658515c53173b86af7281
Validus13 <groundzeroroms@gmail.com>
2014-03-22 17:57:56 -0400

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,3 @@
-add_lunch_combo carbon_d2lte-userdebug
-add_lunch_combo carbon_hammerhead-userdebug
-add_lunch_combo carbon_moto_msm8960dt-userdebug
+add_lunch_combo gzokp_d2lte-userdebug
+add_lunch_combo gzokp_hammerhead-userdebug
+add_lunch_combo gzokp_moto_msm8960dt-userdebug
